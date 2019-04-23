ae0bd01bcd611ac54159f0c015e47d4fd548c9c4
Miguel Maia <migueljunior09@hotmail.com>
2019-04-23 17:29:34 -0300

    create find by id method

--- users/users.model.ts
+++ users/users.model.ts
@@ -1,12 +1,23 @@
 const users = [
-  {name: 'Peter Parker', email: 'peter@marvel.com'},
-  {name: 'Tony Stark', email: 'tony@marvel.com'},
-  {name: 'Bruce Benner', email: 'bruce@marvel.com'},
+  {id: '1',name: 'Peter Parker', email: 'peter@marvel.com'},
+  {id: '2',name: 'Tony Stark', email: 'tony@marvel.com'},
+  {id: '3',name: 'Bruce Benner', email: 'bruce@marvel.com'},
 ]
 
 
 export class User {
   static findAll(): Promise<any[]>{
     return Promise.resolve(users)
+  }
+
+  static findById(id:string): Promise<any>{
+    return new Promise(resolve => {
+      const filtered = users.filter(user => user.id === id)
+      let user = undefined
+      if(filtered.length > 0){
+        user = filtered[0]
+      }
+      resolve(user)
+    })
   }
 }
