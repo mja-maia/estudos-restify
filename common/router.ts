3c74575d770f34cb0c54ca72a00592126f646831
Miguel Maia <migueljunior09@hotmail.com>
2019-04-24 12:14:49 -0300

    refact render login and implement emit event

--- common/router.ts
+++ common/router.ts
@@ -1,5 +1,18 @@
 import * as restify from 'restify'
+import { EventEmitter } from 'events';
 
-export abstract class Router {
+export abstract class Router extends EventEmitter{
   abstract applyRoutes(application : restify.Server) : any
+
+  render(resp: restify.Response, next: restify.Next){
+    return document => {
+      if(document){
+        this.emit('beforeRender', document)
+        resp.json(document)
+      }else{
+        resp.send(404)
+      }
+      return next()
+    }
+  }
 }
