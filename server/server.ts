ea30a4605039c7ee8fd9e07c8afcce85987eea61
Miguel Maia <migueljunior09@hotmail.com>
2019-04-28 16:04:56 -0300

    implements tests

--- server/server.ts
+++ server/server.ts
@@ -1,57 +1,59 @@
 import * as restify from 'restify'
 import * as mongoose from 'mongoose'
-import { mergePatchBodyParser } from './merge-patch.parser'
-import { Router } from '../common/router'
-import { environment } from '../common/environment'
-import { handleError } from './error.handler'
 
+import {environment} from '../common/environment'
+import {Router} from '../common/router'
+import {mergePatchBodyParser} from './merge-patch.parser'
+import {handleError} from './error.handler'
 
 export class Server {
 
-  application!: restify.Server
+  application: restify.Server
 
-  initDb(): mongoose.MongooseThenable{
+  initializeDb(): mongoose.MongooseThenable {
     (<any>mongoose).Promise = global.Promise
     return mongoose.connect(environment.db.url, {
       useMongoClient: true
     })
   }
 
-  initRoutes(routers: Router[]) : Promise<any>{
-    return new Promise((resolve, reject) => {
+  initRoutes(routers: Router[]): Promise<any>{
+    return new Promise((resolve, reject)=>{
       try{
+
         this.application = restify.createServer({
           name: 'meat-api',
           version: '1.0.0'
         })
 
-        //parseia os atributos enviados via query string em atributos isolados
         this.application.use(restify.plugins.queryParser())
-        //parseia os atributos enviados via POST 
         this.application.use(restify.plugins.bodyParser())
-        //bodyParser customizado que foi criado para adicionar 
-        //o contentType nas requisicoes do tipo patch
         this.application.use(mergePatchBodyParser)
-        
+
         //routes
-        for(let router of routers){
+        for (let router of routers) {
           router.applyRoutes(this.application)
         }
-        
-        this.application.listen(environment.server.port, () => {
-          resolve(this.application)
+
+        this.application.listen(environment.server.port, ()=>{
+           resolve(this.application)
         })
 
-        //registro de evento para manipularmos os erros do restify
         this.application.on('restifyError', handleError)
+
       }catch(error){
         reject(error)
       }
     })
   }
 
-  bootstrap(routers : Router[] = []): Promise<Server> {
-    return this.initDb().then(()=>
-    this.initRoutes(routers).then(()=> this))
+  bootstrap(routers: Router[] = []): Promise<Server>{
+      return this.initializeDb().then(()=>
+             this.initRoutes(routers).then(()=> this))
   }
+
+  shutdown(){
+    return mongoose.disconnect().then(()=>this.application.close())
+  }
+
 }
