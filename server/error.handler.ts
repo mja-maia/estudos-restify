9ab5bd817373ec861ad0aad846d60fc465d88157
Miguel Maia <migueljunior09@hotmail.com>
2019-04-28 16:39:15 -0300

    improve error handler

--- server/error.handler.ts
+++ server/error.handler.ts
@@ -21,6 +21,7 @@ export const handleError = (req: restify.Request,
           messages.push({message: err.errors[name].message})
         }
         err.toJSON = () => ({
+          message: 'Validation error while processing your request',
           errors: messages
         })
         break;
